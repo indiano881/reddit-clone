d2aa2e387f6b613f24581e1f929186260832dc4d
davidebaldi <d.baldi88@gmail.com>
2024-10-17 09:48:42 +0200

    types added and script as well v1,30

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { createClient } from "../../utils/supabase/client";
 
 
-export default function Home() {
+export default async function Home() {
+  const supabase= createClient();
+  const {data, error}= await supabase.from("posts").select("*")
+  console.log({data, error})
   return (
     <h1 className="text-5xl">connected</h1>
   );
