4624521571400138df3a18e42de6467b4924295e
davidebaldi <d.baldi88@gmail.com>
2024-10-27 06:06:48 +0100

    server actions added for sign up and works, it console log in the terminal user

---
+++ actions/sign-up.ts
@@ -0,0 +1,19 @@
+"use server"
+
+import { createClient } from "../utils/supabase/server";
+
+export const signUp = async (formData: FormData) => {
+    const data= {
+        //referes to name in the form
+        email: formData.get("email") as string, //type casting
+        password: formData.get("password") as string
+    }
+    //after getting the dta we need to send the data
+
+    const supabase = createClient();
+
+    const {data: {user}, error}= await supabase.auth.signUp(data);
+
+    console.log({user, error})
+
+}
--- src/app/auth/sign-up/form.tsx
+++ src/app/auth/sign-up/form.tsx
@@ -1,6 +1,8 @@
+import { signUp } from "../../../../actions/sign-up"
+
 export const SignUpForm = () => {
     return (
-        <form className="bg-pink-400 flex flex-col m-20 p-8 border-4 w-[50%]">
+        <form action={signUp} className="bg-pink-400 flex flex-col m-20 p-8 border-4 w-[50%]">
         <label htmlFor="email">Email</label>
         <input type="email" name="email" id="email" required />
 
