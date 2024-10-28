b90a72817cabcf9c527b7a4652eeeb6decff5c9b
davidebaldi <d.baldi88@gmail.com>
2024-10-28 11:15:29 +0100

    protected path done

---
+++ src/app/create/page.tsx
@@ -0,0 +1,5 @@
+ const CreatePage=()=> {
+    return <h1 className="bg-zinc-500">Create Page </h1>
+}
+
+export default CreatePage;
--- src/middleware.ts
+++ src/middleware.ts
@@ -24,7 +24,15 @@ export const middleware= async (request: NextRequest) => {
         }
     )
 
-    const {data}= await supabase.auth.getUser();
-    console.log(data)
+    const {data: {user}}= await supabase.auth.getUser();
+    
+    if(!user && protectedRoutes.includes(request.nextUrl.pathname)) {
+        const url=request.nextUrl.clone();
+        url.pathname= "/auth/log-in"
+        return NextResponse.redirect(url)
+    }
+    
     return supabaseResponse;
 }
+
+const protectedRoutes= ["/create"]
