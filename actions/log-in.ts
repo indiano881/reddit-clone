2b4c37a014846e148548a62451c4e78f5212329f
davidebaldi <d.baldi88@gmail.com>
2024-10-28 14:11:05 +0100

    save 3v 1,07,30

--- actions/log-in.ts
+++ actions/log-in.ts
@@ -1,17 +1,14 @@
 "use server"
-
+import {z} from "zod"
 import { redirect } from "next/navigation";
 import { createClient } from "../utils/supabase/server"
 import { logInSchema } from "./schema";
 
-export const logIn= async (formData: FormData) => {
+export const logIn= async (data: z.infer<typeof logInSchema>) => {
     const supabase= createClient();
 
-    const data= {
-        email: formData.get("email"),
-        password: formData.get("password")
-    }
-    const parsedData= logInSchema.parse(data);
+    const parsedData= logInSchema.parse(data)
+
     const {error}= await supabase.auth.signInWithPassword(parsedData)
     if (error) {
        throw error
--- src/app/auth/log-in/form.tsx
+++ src/app/auth/log-in/form.tsx
@@ -4,15 +4,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { logIn } from "../../../../actions/log-in"
 import {useForm} from "react-hook-form";
 import { logInSchema } from "../../../../actions/schema";
+import { z } from "zod";
 
 export const LogInForm = () => {
-    const {register, handleSubmit} = useForm({
+    const {register, handleSubmit} = useForm<z.infer<typeof logInSchema>>({
         resolver: zodResolver(logInSchema)
     })
 
 
     return (
-        <form onSubmit={handleSubmit((values)=> console.log(values), (error)=> console.log(error) )} className="bg-pink-400 flex flex-col m-20 p-8 border-4 w-[50%]">
+        <form onSubmit={handleSubmit((values)=> logIn(values) )} className="bg-pink-400 flex flex-col m-20 p-8 border-4 w-[50%]">
         <label htmlFor="email">Email</label>
         <input {...register("email")} name="email" id="email" required />
 
