feeec94c48e2f206ef13131436c95f4f400d8240
davidebaldi <d.baldi88@gmail.com>
2024-11-04 20:06:25 +0100

    server action logic to delete comment done

---
+++ actions/delete-comments.ts
@@ -0,0 +1,26 @@
+"use server"
+
+import { createClient } from "../utils/supabase/server"
+
+
+const deleteComments= async(commentId: string, postId: string) => {
+    const supabase= createClient();
+    const {data: {user}}= await supabase.auth.getUser()
+    const {data: commentAuthor} = await supabase.from("comments").select("user_id").eq("user_id", commentId).single();
+    const {data: postAuthor}= await supabase.from("posts").select("user_id").eq("id", postId).single();
+
+    const isAuthorPost= user && user.id=== postAuthor?.user_id;
+    const isAuthorComment= user && user.id=== commentAuthor?.user_id;
+
+    if (!isAuthorPost || !isAuthorComment) {
+        throw new Error ("you are not allowed to delete this post!")
+    }
+
+    const {error}=await supabase.from("comments").delete().eq("id", commentId)
+
+    if (error) {
+        throw error
+    }
+}
+
+export default deleteComments;
