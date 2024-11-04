15acfbaf619bb9cb6e6f345f802509c2200e913f
davidebaldi <d.baldi88@gmail.com>
2024-11-04 10:40:04 +0100

    error handling and toast added for edit posts

--- src/app/post/[slug]/edit/form.tsx
+++ src/app/post/[slug]/edit/form.tsx
@@ -7,15 +7,19 @@ import { z } from "zod";
 import { type Tables } from "../../../../../utils/supabase/database.types";
 import { useMutation } from "@tanstack/react-query";
 import { editPost } from "../../../../../actions/edit-post";
+import { error } from "console";
+import { toast } from "sonner";
 
 const EditPostForm = ({defaultValues, postId}: {defaultValues: Pick<Tables<"posts">, "title" | "content">, postId: string})=> {
 
-    const {mutate} = useMutation({
-        mutationFn: editPost
+    const {mutate, isPending} = useMutation({
+        mutationFn: editPost,
+        onError: (error)=> toast.error(error.message),
+        onSuccess:()=> toast.success("your post was uupdated!")
     }
     )
    
-    const {register, handleSubmit} = useForm<z.infer<typeof postSchema>>({
+    const {register, handleSubmit, formState: {errors}} = useForm<z.infer<typeof postSchema>>({
         resolver: zodResolver(postSchema),
         defaultValues: {
             title: defaultValues.title,
@@ -29,9 +33,11 @@ const EditPostForm = ({defaultValues, postId}: {defaultValues: Pick<Tables<"post
             <form onSubmit={handleSubmit((data)=> mutate({data, postId}))} className="flex w-full flex-col gap-4 m-20 bg-orange-800 border-2 border-black">
                 <label htmlFor="title">title</label>
                 <input type="text" id="title"  {...register("title")}/>
+                {errors.title && <p> {errors.title.message}</p>}
                 <label htmlFor="content">content</label>
                 <input type="text" id="content" {...register("content")}/>
-                <button type="submit" className="bg-purple-400">Edit post</button>
+                {errors.content && <p> {errors.content.message}</p>}
+                <button type="submit" className="bg-purple-400">{isPending ? "Editing post" : "Edit post"}</button>
             </form>
         </div>
     )
