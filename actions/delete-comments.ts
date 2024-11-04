2a68fe0dac2001e077ebbaeb182fb1ce65fbc23c
davidebaldi <d.baldi88@gmail.com>
2024-11-04 20:38:50 +0100

    delete comments completed

--- actions/delete-comments.ts
+++ actions/delete-comments.ts
@@ -8,14 +8,14 @@ const deleteComments= async(commentId: number, postId: string) => {
     const supabase= createClient();
     const {data: {user}}= await supabase.auth.getUser()
     const {data: commentAuthor} = await supabase.from("comments").select("user_id").eq("id", commentId).single();
-   // const {data: postAuthor}= await supabase.from("posts").select("user_id").eq("id", postId).single();
+   const {data: postAuthor}= await supabase.from("posts").select("user_id").eq("id", postId).single();
 
-    //const isAuthorPost= user && user.id=== postAuthor?.user_id;
+    const isAuthorPost= user && user.id=== postAuthor?.user_id;
     const isAuthorComment= user && user.id=== commentAuthor?.user_id;
-/*
+
     if (!isAuthorPost && !isAuthorComment) {
         throw new Error ("you are not allowed to delete this post!")
-    }*/
+    }
 
     const {error}=await supabase.from("comments").delete().eq("id", commentId)
     console.log("commentid"+commentId)
