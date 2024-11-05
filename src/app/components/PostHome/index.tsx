765e2e0c0f8055433ccd51389146f7a012f41d4f
davidebaldi <d.baldi88@gmail.com>
2024-11-05 08:56:22 +0100

    styled the articles

--- src/app/components/PostHome/index.tsx
+++ src/app/components/PostHome/index.tsx
@@ -12,12 +12,10 @@ const HomePost = ({post}:{post:PostType}) => {
 
   
     return(
-        <div className="bg-blue-400 min-h-10 m-10 pl-20">
+        <div className="flex flex-col bg-white w-[300px] sm:w-[500px] md:w-[600px] min-h-10 m-4 pl-20 rounded-2xl border-slate-300 border-2 hover:border-black">
         <Link href={`/post/${post.slug}`}>
-        <h1>{post.title}</h1>
-        <h3>slug={post.slug}</h3>
-        <h2>{post.id}</h2>
-        {post.users?.email ? <h6>{post.users.email}</h6> : <h6>Anonymous user</h6>}
+        <h1 className="text-2xl p-2">{post.title}</h1>
+        {post.users?.email ? <h6 className="p-2">{post.users.email}</h6> : <h6>Anonymous user</h6>}
         
         </Link>
         </div>
--- src/app/components/PostsContainer/index.tsx
+++ src/app/components/PostsContainer/index.tsx
@@ -1,22 +1,19 @@
 "use client"
-import { useQuery } from "@tanstack/react-query";
+
 import HomePost from "../PostHome";
-import { getHomePosts } from "../../../../utils/supabase/queries";
-import { revalidatePath } from "next/cache";
 
 export const revalidate= 60*15;
 
 const PostsContainer = ({initialPosts}:{initialPosts:any}) => {
  
-   
     return(
-        <>
+        <div className="flex flex-col items-center">
         {initialPosts.length===0 ? (
         <div>no posts availbale</div>
         ): (
             initialPosts.map((item:any, index:any)=> <div key={index}><HomePost post={item}/></div>)
         )}
-        </>
+        </div>
     )
 }
 
